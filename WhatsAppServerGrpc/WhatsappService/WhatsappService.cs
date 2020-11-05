c3911a901625951ac9de78300d3ab56e9f3ccff5
Eden Yosef <eden-yosef@campus.technion.ac.il>
2020-11-05 15:35:47 +0200

    Add project files.

---
+++ WhatsAppServerGrpc/WhatsappService/WhatsappService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatsAppServerGrpc.Protos;
+
+namespace WhatsAppServerGrpc.WhatsappService
+{
+    public class WhatsappService : WhatsAppMsgService.WhatsAppMsgServiceBase
+    {
+        public override async Task SendInitMessage(InitialMessage request, IServerStreamWriter<ReadingMessage> responseStream, ServerCallContext context)
+        {
+            for(int i=0; i< 10;i++)
+            {
+                await Task.Delay(500);
+
+                await responseStream.WriteAsync(new ReadingMessage() { Text = "this is a test" });
+            }
+        }
+    }
+}
